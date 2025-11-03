f50dbbb7ce2241f09ec1939cb66998a2205a7976
podmefom <uuuuuu.uuuuuu.9@mail.ru>
2025-11-03 15:34:28 +0300

    1

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function HomePage() {
+    return (
+      <>
+        <div>
+          <p>Всем Привет суккккк</p>
+        </div>
+      </>
+    )
+  }
+  
+  export default HomePage
